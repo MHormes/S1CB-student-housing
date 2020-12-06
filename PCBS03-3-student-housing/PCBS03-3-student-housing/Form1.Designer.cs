e2a9c3862d59400614c72a78aa60f94512abcb33
Alessandro Busacchi <I459847@fhict.nl>
2020-12-06 19:45:41 +0100

    login page done

--- PCBS03-3-student-housing/PCBS03-3-student-housing/Form1.Designer.cs
+++ PCBS03-3-student-housing/PCBS03-3-student-housing/Form1.Designer.cs
@@ -29,13 +29,97 @@ namespace PCBS03_3_student_housing
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label_loginTitle = new System.Windows.Forms.Label();
+            this.textBox_accountName = new System.Windows.Forms.TextBox();
+            this.label_accountName = new System.Windows.Forms.Label();
+            this.label_accountPassword = new System.Windows.Forms.Label();
+            this.textBox_accountPassword = new System.Windows.Forms.TextBox();
+            this.button_login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_loginTitle
+            // 
+            this.label_loginTitle.AutoSize = true;
+            this.label_loginTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label_loginTitle.Location = new System.Drawing.Point(267, 76);
+            this.label_loginTitle.Name = "label_loginTitle";
+            this.label_loginTitle.Size = new System.Drawing.Size(253, 29);
+            this.label_loginTitle.TabIndex = 0;
+            this.label_loginTitle.Text = "STUDENT HOUSING";
+            // 
+            // textBox_accountName
+            // 
+            this.textBox_accountName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox_accountName.Location = new System.Drawing.Point(424, 141);
+            this.textBox_accountName.Name = "textBox_accountName";
+            this.textBox_accountName.Size = new System.Drawing.Size(134, 30);
+            this.textBox_accountName.TabIndex = 1;
+            // 
+            // label_accountName
+            // 
+            this.label_accountName.AutoSize = true;
+            this.label_accountName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label_accountName.Location = new System.Drawing.Point(228, 140);
+            this.label_accountName.Name = "label_accountName";
+            this.label_accountName.Size = new System.Drawing.Size(183, 29);
+            this.label_accountName.TabIndex = 2;
+            this.label_accountName.Text = "Account name:";
+            // 
+            // label_accountPassword
+            // 
+            this.label_accountPassword.AutoSize = true;
+            this.label_accountPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label_accountPassword.Location = new System.Drawing.Point(228, 188);
+            this.label_accountPassword.Name = "label_accountPassword";
+            this.label_accountPassword.Size = new System.Drawing.Size(131, 29);
+            this.label_accountPassword.TabIndex = 3;
+            this.label_accountPassword.Text = "Password:";
+            // 
+            // textBox_accountPassword
+            // 
+            this.textBox_accountPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox_accountPassword.Location = new System.Drawing.Point(424, 189);
+            this.textBox_accountPassword.Name = "textBox_accountPassword";
+            this.textBox_accountPassword.Size = new System.Drawing.Size(134, 30);
+            this.textBox_accountPassword.TabIndex = 4;
+            // 
+            // button_login
+            // 
+            this.button_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button_login.Location = new System.Drawing.Point(335, 248);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(123, 35);
+            this.button_login.TabIndex = 5;
+            this.button_login.Text = "LOGIN";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.textBox_accountPassword);
+            this.Controls.Add(this.label_accountPassword);
+            this.Controls.Add(this.label_accountName);
+            this.Controls.Add(this.textBox_accountName);
+            this.Controls.Add(this.label_loginTitle);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label_loginTitle;
+        private System.Windows.Forms.TextBox textBox_accountName;
+        private System.Windows.Forms.Label label_accountName;
+        private System.Windows.Forms.Label label_accountPassword;
+        private System.Windows.Forms.TextBox textBox_accountPassword;
+        private System.Windows.Forms.Button button_login;
     }
 }
 
--- PCBS03-3-student-housing/PCBS03-3-student-housing/Form1.cs
+++ PCBS03-3-student-housing/PCBS03-3-student-housing/Form1.cs
@@ -12,9 +12,57 @@ namespace PCBS03_3_student_housing
 {
     public partial class Form1 : Form
     {
+        private string adminNamePass = "admin";
+        private string studentNamePass = "student";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(textBox_accountName.Text) || string.IsNullOrEmpty(textBox_accountPassword.Text)) {
+                MessageBox.Show("Account name and password required. Please retry.");
+                return;
+            }
+
+            if (textBox_accountName.Text == adminNamePass)
+            {
+                if(textBox_accountPassword.Text == adminNamePass) {
+                    MessageBox.Show("Success");
+                }
+                else {
+                    errorPassword();
+                    return;
+                }
+            }
+            else if (textBox_accountName.Text == studentNamePass)
+            {
+                if (textBox_accountPassword.Text == studentNamePass) {
+                    MessageBox.Show("Success");
+                }
+                else {
+                    errorPassword();
+                    return;
+                }
+            }
+            else {
+                errorAccount();
+                return;
+            }
+        }
+
+        private void errorAccount()
+        {
+            MessageBox.Show("The account doesn't exist. Please retry.");
+            return;
+        }
+
+        private void errorPassword()
+        {
+            MessageBox.Show("Wrong password. Please retry.");
+            return;
         }
     }
 }
