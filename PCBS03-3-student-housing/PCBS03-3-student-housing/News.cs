3937f8836e285c20e4182e9d95cffe86fd29ba4a
Maarten <maarten.hormes@gmail.com>
2020-12-07 12:20:37 +0100

    Updated AdminNews page

---
+++ PCBS03-3-student-housing/PCBS03-3-student-housing/News.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCBS03_3_student_housing
+{
+    class News
+    {
+        private string message;
+        private string author;
+
+        public bool SetNews(string news, string name)
+        {
+            if(!String.IsNullOrWhiteSpace(news) && !String.IsNullOrWhiteSpace(name))
+            { 
+                message = news;
+                author = name;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public string GetNews()
+        {
+            return $"{message} - {author}";
+        }
+    }
+}
--- PCBS03-3-student-housing/PCBS03-3-student-housing/adminForm.Designer.cs
+++ PCBS03-3-student-housing/PCBS03-3-student-housing/adminForm.Designer.cs
@@ -30,50 +30,121 @@ namespace PCBS03_3_student_housing
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tpAdminNews = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lbxAdminNews = new System.Windows.Forms.ListBox();
+            this.tbxMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAddNews = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxAuthorName = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
+            this.tpAdminNews.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tpAdminNews);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(61, 22);
+            this.tabControl1.Location = new System.Drawing.Point(11, 11);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(677, 348);
+            this.tabControl1.Size = new System.Drawing.Size(578, 344);
             this.tabControl1.TabIndex = 1;
             // 
-            // tabPage1
+            // tpAdminNews
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(669, 319);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tpAdminNews.Controls.Add(this.tbxAuthorName);
+            this.tpAdminNews.Controls.Add(this.label2);
+            this.tpAdminNews.Controls.Add(this.btnAddNews);
+            this.tpAdminNews.Controls.Add(this.label1);
+            this.tpAdminNews.Controls.Add(this.tbxMessage);
+            this.tpAdminNews.Controls.Add(this.lbxAdminNews);
+            this.tpAdminNews.Location = new System.Drawing.Point(4, 22);
+            this.tpAdminNews.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tpAdminNews.Name = "tpAdminNews";
+            this.tpAdminNews.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tpAdminNews.Size = new System.Drawing.Size(570, 318);
+            this.tpAdminNews.TabIndex = 0;
+            this.tpAdminNews.Text = "Newsletter";
+            this.tpAdminNews.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(669, 319);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(570, 318);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // lbxAdminNews
+            // 
+            this.lbxAdminNews.FormattingEnabled = true;
+            this.lbxAdminNews.Location = new System.Drawing.Point(6, 6);
+            this.lbxAdminNews.Name = "lbxAdminNews";
+            this.lbxAdminNews.Size = new System.Drawing.Size(318, 303);
+            this.lbxAdminNews.TabIndex = 0;
+            // 
+            // tbxMessage
+            // 
+            this.tbxMessage.AcceptsReturn = true;
+            this.tbxMessage.Location = new System.Drawing.Point(330, 22);
+            this.tbxMessage.Multiline = true;
+            this.tbxMessage.Name = "tbxMessage";
+            this.tbxMessage.Size = new System.Drawing.Size(235, 132);
+            this.tbxMessage.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(330, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Write your message below";
+            // 
+            // btnAddNews
+            // 
+            this.btnAddNews.Location = new System.Drawing.Point(333, 286);
+            this.btnAddNews.Name = "btnAddNews";
+            this.btnAddNews.Size = new System.Drawing.Size(232, 23);
+            this.btnAddNews.TabIndex = 3;
+            this.btnAddNews.Text = "Add your message to the news board";
+            this.btnAddNews.UseVisualStyleBackColor = true;
+            this.btnAddNews.Click += new System.EventHandler(this.btnAddNews_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(333, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Message written by";
+            // 
+            // tbxAuthorName
+            // 
+            this.tbxAuthorName.Location = new System.Drawing.Point(331, 178);
+            this.tbxAuthorName.Name = "tbxAuthorName";
+            this.tbxAuthorName.Size = new System.Drawing.Size(234, 20);
+            this.tbxAuthorName.TabIndex = 5;
+            // 
             // adminForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "adminForm";
             this.Text = "adminForm";
             this.tabControl1.ResumeLayout(false);
+            this.tpAdminNews.ResumeLayout(false);
+            this.tpAdminNews.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -81,7 +152,13 @@ namespace PCBS03_3_student_housing
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tpAdminNews;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox tbxAuthorName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddNews;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxMessage;
+        private System.Windows.Forms.ListBox lbxAdminNews;
     }
 }
