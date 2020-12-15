5f7501a7b2a4ece3ce2fcd550b1013e22403f40b
Luca Pulvirenti <455594@student.fontys.nl>
2020-12-15 14:56:46 +0100

    Added and started to work on new Quit Dialog form

---
+++ PCBS03-3-student-housing/PCBS03-3-student-housing/QuitDialog.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace PCBS03_3_student_housing
+{
+    partial class QuitDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWarningSymbol = new System.Windows.Forms.Label();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.pnlBackground = new System.Windows.Forms.Panel();
+            this.textBox_defocus = new System.Windows.Forms.TextBox();
+            this.label_loginTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlBackground.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblWarningSymbol
+            // 
+            this.lblWarningSymbol.AutoSize = true;
+            this.lblWarningSymbol.Font = new System.Drawing.Font("Arial Black", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWarningSymbol.ForeColor = System.Drawing.Color.White;
+            this.lblWarningSymbol.Location = new System.Drawing.Point(189, 40);
+            this.lblWarningSymbol.Name = "lblWarningSymbol";
+            this.lblWarningSymbol.Size = new System.Drawing.Size(296, 211);
+            this.lblWarningSymbol.TabIndex = 0;
+            this.lblWarningSymbol.Text = "⚠";
+            // 
+            // btnNo
+            // 
+            this.btnNo.Location = new System.Drawing.Point(347, 387);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(152, 68);
+            this.btnNo.TabIndex = 1;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = true;
+            // 
+            // btnYes
+            // 
+            this.btnYes.Location = new System.Drawing.Point(155, 387);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(152, 68);
+            this.btnYes.TabIndex = 1;
+            this.btnYes.Text = "Yes";
+            this.btnYes.UseVisualStyleBackColor = true;
+            // 
+            // pnlBackground
+            // 
+            this.pnlBackground.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(76)))), ((int)(((byte)(207)))));
+            this.pnlBackground.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlBackground.Controls.Add(this.label_loginTitle);
+            this.pnlBackground.Controls.Add(this.btnNo);
+            this.pnlBackground.Controls.Add(this.btnYes);
+            this.pnlBackground.Controls.Add(this.textBox_defocus);
+            this.pnlBackground.Controls.Add(this.label1);
+            this.pnlBackground.Controls.Add(this.lblWarningSymbol);
+            this.pnlBackground.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pnlBackground.Location = new System.Drawing.Point(2, 0);
+            this.pnlBackground.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlBackground.Name = "pnlBackground";
+            this.pnlBackground.Size = new System.Drawing.Size(673, 509);
+            this.pnlBackground.TabIndex = 8;
+            this.pnlBackground.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlBackground_Paint);
+            // 
+            // textBox_defocus
+            // 
+            this.textBox_defocus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
+            this.textBox_defocus.Font = new System.Drawing.Font("Microsoft Sans Serif", 1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_defocus.Location = new System.Drawing.Point(0, 0);
+            this.textBox_defocus.Name = "textBox_defocus";
+            this.textBox_defocus.Size = new System.Drawing.Size(0, 11);
+            this.textBox_defocus.TabIndex = 0;
+            this.textBox_defocus.TabStop = false;
+            // 
+            // label_loginTitle
+            // 
+            this.label_loginTitle.AutoSize = true;
+            this.label_loginTitle.BackColor = System.Drawing.Color.Transparent;
+            this.label_loginTitle.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold);
+            this.label_loginTitle.ForeColor = System.Drawing.Color.White;
+            this.label_loginTitle.Location = new System.Drawing.Point(80, 264);
+            this.label_loginTitle.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label_loginTitle.Name = "label_loginTitle";
+            this.label_loginTitle.Size = new System.Drawing.Size(514, 51);
+            this.label_loginTitle.TabIndex = 0;
+            this.label_loginTitle.Text = "Are you sure you want to";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(81, 305);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(466, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "close this application?";
+            // 
+            // QuitDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 508);
+            this.ControlBox = false;
+            this.Controls.Add(this.pnlBackground);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "QuitDialog";
+            this.Text = "QuitDialog";
+            this.pnlBackground.ResumeLayout(false);
+            this.pnlBackground.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWarningSymbol;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Panel pnlBackground;
+        private System.Windows.Forms.TextBox textBox_defocus;
+        private System.Windows.Forms.Label label_loginTitle;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ PCBS03-3-student-housing/PCBS03-3-student-housing/QuitDialog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+
+namespace PCBS03_3_student_housing
+{
+    public partial class QuitDialog : Form
+    {
+        public QuitDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void pnlBackground_Paint(object sender, PaintEventArgs e)
+        {
+            //creates a color gradient as the background to make the form more visually appealing
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle,
+                                                           Color.FromArgb(138, 92, 251),
+                                                           Color.FromArgb(83, 47, 171),
+                                                           90F))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+
+            //creates a border in order to better differenciate the form from any other applications behind it
+            int borderThickness = 4;
+            using (Pen p = new Pen(Color.FromArgb(66, 66, 66), borderThickness))
+            {
+                e.Graphics.DrawRectangle(p, new Rectangle((borderThickness / 2),
+                                                          (borderThickness / 2),
+                                                          pnlBackground.ClientSize.Width - borderThickness,
+                                                          pnlBackground.ClientSize.Height - borderThickness));
+            }
+        }
+    }
+}
