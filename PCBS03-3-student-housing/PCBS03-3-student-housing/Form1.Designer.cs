08d054acdda5b1808dc926507754076a5146bfb6
Alessandro Busacchi <I459847@fhict.nl>
2021-01-17 16:32:20 +0100

    easy access buttons removed

--- PCBS03-3-student-housing/PCBS03-3-student-housing/Form1.Designer.cs
+++ PCBS03-3-student-housing/PCBS03-3-student-housing/Form1.Designer.cs
@@ -35,8 +35,6 @@ namespace PCBS03_3_student_housing
             this.button_login = new System.Windows.Forms.Button();
             this.lblClose = new System.Windows.Forms.Label();
             this.pnlBackground = new System.Windows.Forms.Panel();
-            this.btnStudent = new System.Windows.Forms.Button();
-            this.btnAdmin = new System.Windows.Forms.Button();
             this.textBox_defocus = new System.Windows.Forms.TextBox();
             this.pnlBar = new System.Windows.Forms.Panel();
             this.pnlDragBar = new System.Windows.Forms.Panel();
@@ -50,10 +48,9 @@ namespace PCBS03_3_student_housing
             this.label_loginTitle.BackColor = System.Drawing.Color.Transparent;
             this.label_loginTitle.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label_loginTitle.ForeColor = System.Drawing.Color.White;
-            this.label_loginTitle.Location = new System.Drawing.Point(187, 135);
-            this.label_loginTitle.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label_loginTitle.Location = new System.Drawing.Point(107, 74);
             this.label_loginTitle.Name = "label_loginTitle";
-            this.label_loginTitle.Size = new System.Drawing.Size(522, 76);
+            this.label_loginTitle.Size = new System.Drawing.Size(289, 42);
             this.label_loginTitle.TabIndex = 0;
             this.label_loginTitle.Text = "Student Housing";
             // 
@@ -61,10 +58,9 @@ namespace PCBS03_3_student_housing
             // 
             this.textBox_accountName.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox_accountName.ForeColor = System.Drawing.Color.Gray;
-            this.textBox_accountName.Location = new System.Drawing.Point(200, 261);
-            this.textBox_accountName.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox_accountName.Location = new System.Drawing.Point(114, 144);
             this.textBox_accountName.Name = "textBox_accountName";
-            this.textBox_accountName.Size = new System.Drawing.Size(487, 70);
+            this.textBox_accountName.Size = new System.Drawing.Size(280, 42);
             this.textBox_accountName.TabIndex = 0;
             this.textBox_accountName.TabStop = false;
             this.textBox_accountName.Text = "Account name";
@@ -77,10 +73,9 @@ namespace PCBS03_3_student_housing
             // 
             this.textBox_accountPassword.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox_accountPassword.ForeColor = System.Drawing.Color.Gray;
-            this.textBox_accountPassword.Location = new System.Drawing.Point(200, 362);
-            this.textBox_accountPassword.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox_accountPassword.Location = new System.Drawing.Point(114, 200);
             this.textBox_accountPassword.Name = "textBox_accountPassword";
-            this.textBox_accountPassword.Size = new System.Drawing.Size(487, 70);
+            this.textBox_accountPassword.Size = new System.Drawing.Size(280, 42);
             this.textBox_accountPassword.TabIndex = 0;
             this.textBox_accountPassword.TabStop = false;
             this.textBox_accountPassword.Text = "Password";
@@ -93,10 +88,9 @@ namespace PCBS03_3_student_housing
             // 
             this.button_login.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_login.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(76)))), ((int)(((byte)(207)))));
-            this.button_login.Location = new System.Drawing.Point(333, 512);
-            this.button_login.Margin = new System.Windows.Forms.Padding(5);
+            this.button_login.Location = new System.Drawing.Point(190, 282);
             this.button_login.Name = "button_login";
-            this.button_login.Size = new System.Drawing.Size(215, 63);
+            this.button_login.Size = new System.Drawing.Size(123, 35);
             this.button_login.TabIndex = 0;
             this.button_login.TabStop = false;
             this.button_login.Text = "Login";
@@ -108,10 +102,10 @@ namespace PCBS03_3_student_housing
             this.lblClose.Cursor = System.Windows.Forms.Cursors.Hand;
             this.lblClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblClose.ForeColor = System.Drawing.Color.White;
-            this.lblClose.Location = new System.Drawing.Point(823, 1);
-            this.lblClose.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblClose.Location = new System.Drawing.Point(470, 1);
+            this.lblClose.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblClose.Name = "lblClose";
-            this.lblClose.Size = new System.Drawing.Size(63, 63);
+            this.lblClose.Size = new System.Drawing.Size(36, 35);
             this.lblClose.TabIndex = 6;
             this.lblClose.Text = "✖";
             this.lblClose.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -122,8 +116,6 @@ namespace PCBS03_3_student_housing
             // pnlBackground
             // 
             this.pnlBackground.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pnlBackground.Controls.Add(this.btnStudent);
-            this.pnlBackground.Controls.Add(this.btnAdmin);
             this.pnlBackground.Controls.Add(this.textBox_defocus);
             this.pnlBackground.Controls.Add(this.pnlBar);
             this.pnlBackground.Controls.Add(this.button_login);
@@ -132,51 +124,22 @@ namespace PCBS03_3_student_housing
             this.pnlBackground.Controls.Add(this.textBox_accountName);
             this.pnlBackground.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.pnlBackground.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.pnlBackground.Location = new System.Drawing.Point(-3, 0);
-            this.pnlBackground.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlBackground.Location = new System.Drawing.Point(-2, 0);
+            this.pnlBackground.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnlBackground.Name = "pnlBackground";
-            this.pnlBackground.Size = new System.Drawing.Size(890, 645);
+            this.pnlBackground.Size = new System.Drawing.Size(509, 357);
             this.pnlBackground.TabIndex = 7;
             this.pnlBackground.Click += new System.EventHandler(this.pnlBackground_Click);
             this.pnlBackground.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlBackground_Paint);
-            // 
-            // btnStudent
-            // 
-            this.btnStudent.BackColor = System.Drawing.Color.Transparent;
-            this.btnStudent.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStudent.ForeColor = System.Drawing.Color.White;
-            this.btnStudent.Location = new System.Drawing.Point(14, 583);
-            this.btnStudent.Name = "btnStudent";
-            this.btnStudent.Size = new System.Drawing.Size(357, 45);
-            this.btnStudent.TabIndex = 0;
-            this.btnStudent.TabStop = false;
-            this.btnStudent.Text = "Student quick access (dev)";
-            this.btnStudent.UseVisualStyleBackColor = false;
-            this.btnStudent.Click += new System.EventHandler(this.btnStudent_Click);
-            // 
-            // btnAdmin
-            // 
-            this.btnAdmin.BackColor = System.Drawing.Color.Transparent;
-            this.btnAdmin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdmin.ForeColor = System.Drawing.Color.White;
-            this.btnAdmin.Location = new System.Drawing.Point(536, 583);
-            this.btnAdmin.Name = "btnAdmin";
-            this.btnAdmin.Size = new System.Drawing.Size(341, 45);
-            this.btnAdmin.TabIndex = 0;
-            this.btnAdmin.TabStop = false;
-            this.btnAdmin.Text = "Admin quick access (dev)";
-            this.btnAdmin.UseVisualStyleBackColor = false;
-            this.btnAdmin.Click += new System.EventHandler(this.btnAdmin_Click);
             // 
             // textBox_defocus
             // 
             this.textBox_defocus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
             this.textBox_defocus.Font = new System.Drawing.Font("Microsoft Sans Serif", 1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox_defocus.Location = new System.Drawing.Point(0, 0);
+            this.textBox_defocus.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox_defocus.Name = "textBox_defocus";
-            this.textBox_defocus.Size = new System.Drawing.Size(0, 11);
+            this.textBox_defocus.Size = new System.Drawing.Size(2, 9);
             this.textBox_defocus.TabIndex = 0;
             this.textBox_defocus.TabStop = false;
             this.textBox_defocus.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_defocus_KeyDown);
@@ -187,10 +150,10 @@ namespace PCBS03_3_student_housing
             this.pnlBar.Controls.Add(this.pnlDragBar);
             this.pnlBar.Controls.Add(this.lblClose);
             this.pnlBar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.pnlBar.Location = new System.Drawing.Point(-2, -2);
-            this.pnlBar.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlBar.Location = new System.Drawing.Point(-1, -1);
+            this.pnlBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnlBar.Name = "pnlBar";
-            this.pnlBar.Size = new System.Drawing.Size(885, 71);
+            this.pnlBar.Size = new System.Drawing.Size(506, 39);
             this.pnlBar.TabIndex = 8;
             // 
             // pnlDragBar
@@ -198,24 +161,23 @@ namespace PCBS03_3_student_housing
             this.pnlDragBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
             this.pnlDragBar.Cursor = System.Windows.Forms.Cursors.Default;
             this.pnlDragBar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.pnlDragBar.Location = new System.Drawing.Point(-2, -2);
-            this.pnlDragBar.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlDragBar.Location = new System.Drawing.Point(-1, -1);
+            this.pnlDragBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnlDragBar.Name = "pnlDragBar";
-            this.pnlDragBar.Size = new System.Drawing.Size(811, 69);
+            this.pnlDragBar.Size = new System.Drawing.Size(463, 38);
             this.pnlDragBar.TabIndex = 9;
             this.pnlDragBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlDragBar_MouseDown);
             this.pnlDragBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlDragBar_MouseMove);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(76)))), ((int)(((byte)(207)))));
-            this.ClientSize = new System.Drawing.Size(887, 641);
+            this.ClientSize = new System.Drawing.Size(507, 354);
             this.ControlBox = false;
             this.Controls.Add(this.pnlBackground);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -237,8 +199,6 @@ namespace PCBS03_3_student_housing
         private System.Windows.Forms.Panel pnlBar;
         private System.Windows.Forms.Panel pnlDragBar;
         private System.Windows.Forms.TextBox textBox_defocus;
-        private System.Windows.Forms.Button btnAdmin;
-        private System.Windows.Forms.Button btnStudent;
     }
 }
 
